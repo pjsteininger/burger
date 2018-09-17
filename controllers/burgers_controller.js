a58f01a329af923d7c2f9fd2bf0f931660fad75c
Philip S(D) <philipsteininger@gmail.com>
2018-09-16 23:15:42 -0700

    routing updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,30 @@
 var express = require("express");
 var burger = require("../models/burger.js")
+var router = express.Router();
+
+router.get("/", function(req, res) {
+
+
+
+
+});
+
+router.post("/api/burgers", function(req, res) {
+
+
+
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+
+
+
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+
+
+
+});
+
+module.exports = router;
